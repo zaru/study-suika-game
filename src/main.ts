6a4ac03e8921aaa3188cbeabd8f84902736bb6a9
zaru <zarutofu@gmail.com>
2023-10-15 19:45:43 +0900

    ボールがぶつかったら消えるように修正

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,3 @@
-// import Phaser from "phaser";
-//
-// class Scene extends Phaser.Scene {
-//   #ground: Phaser.Physics.Arcade.StaticGroup;
-//   #balls: Phaser.Physics.Arcade.Group;
-//
-//   constructor() {
-//     super();
-//   }
-//
-//   preload() {
-//     this.load.json("ballShape", "assets/ball.json");
-//     this.load.image("ball", "assets/ball.png");
-//   }
-//   create() {
-//     this.#balls = this.physics.add.group({
-//       bounceX: 0.4,
-//       bounceY: 0.4,
-//       collideWorldBounds: true,
-//     });
-//
-//     this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
-//       this.createBall(pointer.x, pointer.y);
-//     });
-//   }
-//
-//   private createBall(x: number, y: number) {
-//     const ballShape = this.cache.json.get("ballShape");
-//     const ball = this.physics.add.sprite(x, y, "ball");
-//     ball.body.setCircle(
-//       ballShape.ball.fixtures[0].circle.radius,
-//       ballShape.ball.fixtures[0].circle.x,
-//       ballShape.ball.fixtures[0].circle.y,
-//     );
-//     ball.setBounce(0.6);
-//     ball.setCollideWorldBounds(true);
-//
-//     this.#balls.add(ball);
-//     this.physics.add.collider(ball, this.#balls);
-//   }
-// }
-//
-// const config: Phaser.Types.Core.GameConfig = {
-//   type: Phaser.AUTO,
-//   width: 800,
-//   height: 600,
-//   parent: "app",
-//   physics: {
-//     default: "arcade",
-//     arcade: {
-//       gravity: { y: 1500 },
-//     },
-//   },
-//   scene: Scene,
-// };
-//
-// new Phaser.Game(config);
-
 import Phaser from "phaser";
 
 class Scene extends Phaser.Scene {
@@ -83,11 +25,27 @@ class Scene extends Phaser.Scene {
     this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
       this.createBall(pointer.x, pointer.y);
     });
+
+    this.matter.world.on(
+      "collisionstart",
+      (
+        _: MatterJS.IEventCollision<MatterJS.Engine>,
+        ballA: MatterJS.BodyType,
+        ballB: MatterJS.BodyType,
+      ) => {
+        if (
+          this.#balls.includes(ballA.parent.gameObject) &&
+          this.#balls.includes(ballB.parent.gameObject)
+        ) {
+          ballA.parent.gameObject.destroy();
+        }
+      },
+    );
   }
 
   private createBall(x: number, y: number) {
     const ballShape = this.cache.json.get("ballShape");
-    const ball = this.matter.add.image(x, y, "ball", null, {
+    const ball = this.matter.add.image(x, y, "ball", undefined, {
       shape: ballShape.ball,
       restitution: 0.6,
       friction: 0.01,

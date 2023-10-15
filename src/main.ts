ba29855e24ecc2842b07761c1cac6b9ed221c6c8
zaru <zarutofu@gmail.com>
2023-10-15 20:36:55 +0900

    ボールを動物に変更

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,20 @@ import Phaser from "phaser";
 
 class Scene extends Phaser.Scene {
   #balls: Phaser.Physics.Matter.Image[];
+  #maxTypeNum = 11;
+  #ballSize = {
+    1: 0.1,
+    2: 0.115,
+    3: 0.135,
+    4: 0.155,
+    5: 0.225,
+    6: 0.3,
+    7: 0.4,
+    8: 0.5,
+    9: 0.6,
+    10: 0.7,
+    11: 0.8,
+  };
 
   constructor() {
     super();
@@ -10,10 +24,9 @@ class Scene extends Phaser.Scene {
 
   preload() {
     this.load.json("ballShape", "assets/ball.json");
-    this.load.image("ball1", "assets/ball1.png");
-    this.load.image("ball2", "assets/ball2.png");
-    this.load.image("ball3", "assets/ball3.png");
-    this.load.image("ball4", "assets/ball4.png");
+    for (let i = 1; i <= this.#maxTypeNum; i++) {
+      this.load.image(`ball${i}`, `assets/ball${i}.png`);
+    }
   }
 
   create() {
@@ -25,7 +38,8 @@ class Scene extends Phaser.Scene {
     );
 
     this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
-      this.createBall("ball1", pointer.x, pointer.y);
+      const ballType = `ball${Math.floor(Math.random() * 5) + 1}`;
+      this.createBall(ballType, pointer.x, pointer.y);
     });
 
     this.matter.world.on(
@@ -46,10 +60,13 @@ class Scene extends Phaser.Scene {
           const ballNum = Number(
             ballA.parent.gameObject.getData("ballType").replace("ball", ""),
           );
-          if (ballNum < 5) {
+          if (ballNum < this.#maxTypeNum) {
             ballA.parent.gameObject.destroy();
             ballB.parent.gameObject.destroy();
             this.createBall(`ball${ballNum + 1}`, x, y);
+          } else if (ballNum === this.#maxTypeNum) {
+            ballA.parent.gameObject.destroy();
+            ballB.parent.gameObject.destroy();
           }
         }
       },
@@ -60,9 +77,12 @@ class Scene extends Phaser.Scene {
     const ballShape = this.cache.json.get("ballShape");
     const ball = this.matter.add.image(x, y, ballType, undefined, {
       shape: ballShape[ballType],
-      restitution: 0.6,
+      restitution: 0.5,
       friction: 0.01,
     });
+    const ballNum = Number(ballType.replace("ball", ""));
+    ball.setScale(this.#ballSize[ballNum]);
+
     ball.setData("ballType", ballType);
     this.#balls.push(ball);
   }
@@ -71,7 +91,7 @@ class Scene extends Phaser.Scene {
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
-  height: 600,
+  height: 1200,
   parent: "app",
   physics: {
     default: "matter",
